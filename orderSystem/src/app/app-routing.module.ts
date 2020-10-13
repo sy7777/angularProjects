2ecfc77bd29d3977ed6a70ba8e8f747445b22dc1
sy7777 <piecesy7@gmail.com>
2020-10-13 14:34:11 +1000

    router

--- orderSystem/src/app/app-routing.module.ts
+++ orderSystem/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ProductContentComponent } from './components/product-content/product-content.component';
+
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"product-content", component:ProductContentComponent},
+  {path:"**",redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- orderSystem/src/app/app.module.ts
+++ orderSystem/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProductContentComponent } from './components/product-content/product-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductContentComponent
   ],
   imports: [
     BrowserModule,
